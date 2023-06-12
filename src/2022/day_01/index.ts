72a850f2f836525fd61d7827495fe1f2a4366413
bruno <bruno.resende@gmx.com>
2023-06-12 15:46:00 -0300

    Refactor: Ready for general use

--- src/2022/day_01/index.ts
+++ src/2022/day_01/index.ts
@@ -1,39 +1,36 @@
 import fs from 'fs';
-import { splitOnEmptyLine, splitOnLineBreak } from '../../utils/splitStrings';
+import path from 'path';
 
-function main() {
-    fs.readFile('src/ex_01/calories.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
+const getSumOfGroup = (group: string) =>
+    group.split('\n').reduce((acc, cur) => acc + Number(cur), 0);
+
+function puzzle1() {
+    fs.readFile(
+        path.resolve(__dirname, './calories.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('Could not read the file');
+            const elfCalories = data.split('\n\n').map(getSumOfGroup);
+            const highestCalories = Math.max(...elfCalories);
+            console.log(highestCalories);
         }
-        const elfCalories = splitOnEmptyLine(data);
-        const sum4Elf = elfCalories.map((bulk) =>
-            splitOnLineBreak(bulk).reduce((acc, crr) => acc + Number(crr), 0)
-        );
-
-        const mostCalories = Math.max(...sum4Elf);
-        console.log(mostCalories);
-    });
+    );
 }
 
-function main2() {
-    fs.readFile('src/ex_01/calories.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
+function puzzle2() {
+    fs.readFile(
+        path.resolve(__dirname, './calories.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('Could not read the file');
+            const elfCalories = data.split('\n\n').map(getSumOfGroup);
+            const topCalories = [...elfCalories].sort((a, b) => b - a);
+            const totalTopCalories = topCalories
+                .slice(0, 3)
+                .reduce((acc, crr) => acc + crr, 0);
+            console.log(totalTopCalories);
         }
-        const elfCalories = splitOnEmptyLine(data);
-        const sum4Elf = elfCalories.map((bulk) =>
-            splitOnLineBreak(bulk).reduce((acc, crr) => acc + Number(crr), 0)
-        );
-
-        const topCalories = sum4Elf.sort((a, b) => b - a);
-        const totalTopCalories = topCalories
-            .slice(0, 3)
-            .reduce((acc, crr) => acc + crr, 0);
-        console.log(totalTopCalories);
-    });
+    );
 }
 
-main2();
+puzzle2();
--- src/utils/splitStrings.ts
+++
@@ -1,7 +0,0 @@
-export function splitOnLineBreak(string: string) {
-    return string.split(/\r?\n/);
-}
-
-export function splitOnEmptyLine(string: string) {
-    return string.split('\n\n');
-}
