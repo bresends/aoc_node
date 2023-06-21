41898240816a7305457b6774bac334ed34533f28
bruno <bruno.resende@gmx.com>
2023-06-21 15:35:07 -0300

    Day 4 Done!

---
+++ src/2022/day_04/index.ts
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import path from 'path';
+import { start } from 'repl';
+
+function convertToRange(inputText: string) {
+    // Format: 31-31,32-40 - '()' to capture groups, '\d' for a digit '+' for a sequence of 1+ digits, and the other caracters to match the Format.
+    // Slice to remove the first element of the return array, that is the original string.
+    return inputText.split('\n').map((pair) =>
+        pair
+            .match(/(\d+)-(\d+),(\d+)-(\d+)/)
+            .slice(1)
+            .map(Number)
+    );
+}
+
+function totalOverlapCheck([
+    sec1Start,
+    sec1End,
+    sec2Start,
+    sec2End,
+]: Array<number>) {
+    return (
+        (sec1Start >= sec2Start && sec1End <= sec2End) ||
+        (sec1Start <= sec2Start && sec1End >= sec2End)
+    );
+}
+
+function partialOverlapCheck([
+    sec1Start,
+    sec1End,
+    sec2Start,
+    sec2End,
+]: Array<number>) {
+    return sec1Start <= sec2End && sec2Start <= sec1End;
+}
+
+function puzzle1() {
+    fs.readFile(
+        path.resolve(__dirname, './pairs.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('Could not read the file');
+            const totalOverlaps =
+                convertToRange(data).filter(totalOverlapCheck).length;
+
+            console.log(totalOverlaps);
+        }
+    );
+}
+
+function puzzle2() {
+    fs.readFile(
+        path.resolve(__dirname, './pairs.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('Could not read the file');
+            const totalOverlaps =
+                convertToRange(data).filter(partialOverlapCheck).length;
+            console.log(totalOverlaps);
+        }
+    );
+}
+
+puzzle2();
